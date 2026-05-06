7b4a411abba139eac164cdf3715645c665edf0c1
Longke <ninjalong161@gmail.com>
2026-05-06 14:55:16 +0700

    fix session

--- BE/docker/entrypoint.sh
+++ BE/docker/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 set -e
 
+# Clear any cached config/routes/views so runtime env is always respected.
+php artisan optimize:clear
+
 if [ "${RUN_MIGRATIONS}" = "true" ]; then
   php artisan migrate --force
 fi
